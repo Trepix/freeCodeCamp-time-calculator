3e348f9cb781a0c8e2aff165dc1323aef76f3e42
Trepix <pau.trepat.segura@gmail.com>
2023-03-11 22:49:56 +0100

    Create Clock concept to hold time and format

--- time_calculator.py
+++ time_calculator.py
@@ -1,20 +1,3 @@
-def parse_time(start):
-    time = start.split()[0].split(":")
-    return Time(time[0], time[1])
-
-
-def parse_duration(duration):
-    time = duration.split(":")
-    return Time(time[0], time[1])
-
-
-def add_time(start, duration):
-    clock = parse_time(start)
-    duration = parse_duration(duration)
-    result = clock.add(duration)
-    return f'{result.hours()}:{str(result.minutes())} PM'
-
-
 class Time:
     def __init__(self, hours, minutes):
         self._hour = int(hours)
@@ -28,3 +11,31 @@ class Time:
 
     def add(self, time: 'Time'):
         return Time(self._hour + time._hour, self._minutes + time._minutes)
+
+
+class Clock:
+    def __init__(self, time: Time, clock_format):
+        self._time = time
+        self._format = clock_format
+
+    def add(self, time: Time):
+        return Clock(self._time.add(time), self._format)
+
+
+def parse_clock(start) -> Clock:
+    time = start.split()[0].split(":")
+    return Clock(Time(time[0], time[1]), "PM")
+
+
+def parse_duration(duration):
+    time = duration.split(":")
+    return Time(time[0], time[1])
+
+
+def add_time(start, duration):
+    clock = parse_clock(start)
+    duration = parse_duration(duration)
+    clock = clock.add(duration)
+    result = clock._time
+    clock_format = clock._format
+    return f'{result.hours()}:{str(result.minutes())} {clock_format}'
