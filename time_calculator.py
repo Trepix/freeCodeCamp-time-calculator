492b280d75fcc0d19f5b3b71745954d08373b7ea
Trepix <pau.trepat.segura@gmail.com>
2023-03-12 00:03:36 +0100

    Add minutes rotation preparing next case iteration

--- time_calculator.py
+++ time_calculator.py
@@ -1,16 +1,10 @@
 class Time:
     def __init__(self, hours, minutes):
-        self._hour = int(hours)
-        self._minutes = int(minutes)
-
-    def hours(self):
-        return self._hour
-
-    def minutes(self):
-        return self._minutes
+        self.hours = int(hours)
+        self.minutes = int(minutes)
 
     def add(self, time: 'Time'):
-        return Time(self._hour + time._hour, self._minutes + time._minutes)
+        return Time(self.hours + time.hours, self.minutes + time.minutes)
 
 
 class Clock:
@@ -22,8 +16,10 @@ class Clock:
         return Clock(self._time.add(time), self._format)
 
     def to_string(self):
-        minutes = self._time.minutes()
-        return f'{self._time.hours()}:{minutes:02} {self._format}'
+        minutes = self._time.minutes % 60
+        hours = self._time.hours + int(self._time.minutes / 60)
+        clock_format = self._format
+        return f'{hours}:{minutes:02} {clock_format}'
 
 
 def parse_clock(start) -> Clock:
