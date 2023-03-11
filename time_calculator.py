016b997b9b6c82dc7c13990d322432849a94f1a3
Trepix <pau.trepat.segura@gmail.com>
2023-03-11 15:35:53 +0100

    Introduce Time concept to avoid data clump code smell

--- time_calculator.py
+++ time_calculator.py
@@ -1,2 +1,20 @@
+def parse_time(start):
+    time = start.split()[0].split(":")
+    return Time(time[0], time[1])
+
+
 def add_time(start, duration):
-    return "5:42 PM"
+    time = parse_time(start)
+    return str(time.hours() + 2) + ":" + str(time.minutes() + 12) + " PM"
+
+
+class Time:
+    def __init__(self, hours, minutes):
+        self._hour = hours
+        self._minutes = minutes
+
+    def hours(self):
+        return int(self._hour)
+
+    def minutes(self):
+        return int(self._minutes)
